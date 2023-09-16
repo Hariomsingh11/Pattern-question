124954a34139d63ac222418578b5c628897584b9
Hariomsingh11 <singhhariom1940@gmail.com>
2023-09-16 15:31:35 +0530

    First Commit

---
+++ pattern1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,j;
+    int n=4;
+    while(i<=n){
+        j=1;
+        while (j<=n)
+        {
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+
+    }
+}
